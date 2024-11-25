ba05201255837ad5881fe2bb7ee7a2375bd1ee1b
Max <yymaxverum@gmail.com>
2024-11-25 18:15:04 +0200

    more info in https://www.youtube.com/watch?v=hPr-Yc92qaY

---
+++ main.py
@@ -0,0 +1,12 @@
+import poems
+import sign
+
+name = sign.CONSTANT.format(name='Максим')
+print(name)
+wishes = 'мати найкращій компютер,хочу вміти все'
+star = "**************************************************************************"
+print(star)
+print(poems.FIRST_POEM)
+print(star)
+print(poems.SECOND_CONSTANTA)
+print(star)
